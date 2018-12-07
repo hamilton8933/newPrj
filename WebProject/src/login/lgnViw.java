707f3278236663be9d11e3dee6e3583de8169574
shy04 <shy04@Hamilton>
2018-12-08 01:14:58 +0900

    First Commit Test

---
+++ WebProject/src/login/lgnViw.java
@@ -0,0 +1,20 @@
+package login;
+
+public class lgnViw {
+	public lgnViwOut lgnViw(lgnViwIn in) {
+		
+		lgnViwIn lgnIn = new lgnViwIn();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		lgnViwOut out = new lgnViwOut();
+		
+		return out;
+	}
+}
